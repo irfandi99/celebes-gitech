d61c475ba57880f189213d1401eada367f0348e2
irfandi99 <irfandi@hotmail.com>
2020-12-19 13:25:29 +0900

    menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar Git Remote</title>
+</head>     
+<body>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem sint quae explicabo maxime porro cupiditate assumenda ut sit facere doloribus adipisci, eos corrupti omnis nam rerum quam sunt ipsum at?</p>
+</body>
+</html>
